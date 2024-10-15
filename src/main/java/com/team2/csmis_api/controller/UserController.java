16229fe3f71515ea2c73d35be8388c9a28b6e3c7
kmthein <kmthein96.mm@gmail.com>
2024-10-15 14:49:18 +0630

    role update

--- src/main/java/com/team2/csmis_api/controller/UserController.java
+++ src/main/java/com/team2/csmis_api/controller/UserController.java
@@ -8,6 +8,7 @@ import com.team2.csmis_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +18,8 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/users")
+@RequestMapping("/admin/api/users")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserService userService;
