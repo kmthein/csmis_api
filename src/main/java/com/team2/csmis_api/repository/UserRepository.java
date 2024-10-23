ddb28a90688e7fb5d42defb8cfd02b1e35d2fa51
LaYaungO <layaung92005@gmail.com>
2024-10-23 11:15:32 +0630

    forgotpassword done

--- src/main/java/com/team2/csmis_api/repository/UserRepository.java
+++ src/main/java/com/team2/csmis_api/repository/UserRepository.java
@@ -4,10 +4,14 @@ import com.team2.csmis_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByStaffId(String staffId);
     User findByName(String username);
 
     User findById(Long userId);
+
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/team2/csmis_api/service/EmailService.java
@@ -0,0 +1,27 @@
+package com.team2.csmis_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    public void sendOTP(String email, String otp) {
+        String subject = "Your OTP for Password Reset";
+        String message = "Your OTP for password reset is: " + otp;
+        sendSimpleMessage(email, subject, message);
+    }}
