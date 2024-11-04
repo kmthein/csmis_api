2f1970661d59b010c8dd16d104b6989a3d9c0d97
MyoLei <myoleioo7@gmail.com>
2024-11-04 14:47:49 +0630

    push doorlogs features

---
+++ src/main/java/com/team2/csmis_api/controller/DoorLogController.java
@@ -0,0 +1,39 @@
+package com.team2.csmis_api.controller;
+
+import com.team2.csmis_api.dto.DoorAccessRecordDTO;
+import com.team2.csmis_api.service.DoorLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/doorLogs")
+public class DoorLogController {
+
+    @Autowired
+    private DoorLogService doorLogService;
+
+    @PostMapping("excel")
+    public ResponseEntity<?> uploadHolidaysData(@RequestParam("file") MultipartFile file,
+                                                @RequestParam("adminId") Integer adminId) {
+        doorLogService.saveDoorLogToDatabase(file, adminId);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Map.of("message", "DoorLog data uploaded and saved to database successfully"));
+    }
+
+    @GetMapping("")
+    public List<DoorAccessRecordDTO> getAllDoorLog() {
+        return doorLogService.getAllDoorAccessRecords();
+    }
+
+    @GetMapping("{id}")
+    public DoorAccessRecordDTO getDoorLogById(@PathVariable("id") Integer id) {
+        return doorLogService.getDoorLogById(id);
+    }
+}
--- src/main/java/com/team2/csmis_api/repository/UserRepository.java
+++ src/main/java/com/team2/csmis_api/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.team2.csmis_api.repository;
 
+import com.team2.csmis_api.entity.Department;
 import com.team2.csmis_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     public User getUserById(int id);
     User findById(Long userId);
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT d FROM User d WHERE d.doorLogNo = :doorLogNo")
+    User findByDoorLogNo(@Param("doorLogNo") int doorLogNo);
+
 }
