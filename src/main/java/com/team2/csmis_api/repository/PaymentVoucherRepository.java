ad7287f566cc335ba95c85712b6c8504f34c43e0
htet-hlyan123 <hlyanhtet127@gmail.com>
2024-11-28 23:38:45 +0630

    operator cost weekly & monthly

--- src/main/java/com/team2/csmis_api/repository/PaymentVoucherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team2.csmis_api.repository;
-
-import com.team2.csmis_api.entity.PaymentVoucher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentVoucherRepository extends JpaRepository<PaymentVoucher, Integer> {
-}
--- src/main/java/com/team2/csmis_api/repository/VoucherRowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team2.csmis_api.repository;
-
-import com.team2.csmis_api.entity.VoucherRow;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoucherRowRepository extends JpaRepository<VoucherRow, Long> {
-}
