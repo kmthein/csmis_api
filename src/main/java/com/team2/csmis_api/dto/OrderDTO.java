446d02c8b63f8ba240316c07e794229e2ac77580
kmthein <kmthein96.mm@gmail.com>
2024-11-30 00:53:52 +0630

    order fix

--- src/main/java/com/team2/csmis_api/dto/OrderDTO.java
+++ src/main/java/com/team2/csmis_api/dto/OrderDTO.java
@@ -1,11 +1,15 @@
 package com.team2.csmis_api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
     private Integer id;
     private LocalDate orderDate;
